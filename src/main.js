c7ea255014f5b419506989b17269b1ae43f11e7a
wzr <470557449@qq.com>
2019-06-18 19:17:19 +0800

    完成滑动组件，和歌单列表

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 Vue.config.productionTip = false
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
